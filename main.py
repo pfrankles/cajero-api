79534a1571068fa8b31eec71b2313de35273a96a
pfrankles <fr.gonzalez.c@gmail.com>
2020-12-10 09:00:27 -0500

    actualizacion main

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = FastAPI()
 
 
 from fastapi.middleware.cors import CORSMiddleware
-origins = ["http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8081"]
+origins = ["http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8081", "https://cajero-appfgc.herokuapp.com"]
 api.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 
